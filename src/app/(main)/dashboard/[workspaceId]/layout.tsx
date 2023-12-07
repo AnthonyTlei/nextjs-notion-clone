e18ce1a518bc178ace238df75970108c73aacdbc
Anthony Tleiji <anthonytleiji@gmail.com>
2023-12-07 18:43:19 +0200

    Add mobile sidebar

--- src/app/(main)/dashboard/[workspaceId]/layout.tsx
+++ src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -1,3 +1,4 @@
+import MobileSidebar from "@/components/sidebar/mobile-sidebar";
 import Sidebar from "@/components/sidebar/sidebar";
 import { ReactNode } from "react";
 
@@ -10,6 +11,9 @@ const WorkspaceLayout = ({ children, params }: WorkspaceLayoutProps) => {
   return (
     <main className="flex h-screen w-screen overflow-hidden">
       <Sidebar params={params} />
+      <MobileSidebar>
+        <Sidebar params={params} className="inline-block w-screen sm:hidden" />
+      </MobileSidebar>
       <div className="dark:boder-Neutrals-12/70 relative w-full overflow-scroll border-l-[1px]">
         {children}
       </div>
