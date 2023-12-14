c990333a131695bbd3ff305241b34c4fff7bf2f1
Anthony Tleiji <anthonytleiji@gmail.com>
2023-12-14 12:53:33 +0200

    Setup Stripe

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,3 +22,30 @@ export const getURL = () => {
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
   return url;
 };
+
+export const postData = async ({
+  url,
+  data,
+}: {
+  url: string;
+  data?: { price: Price };
+}) => {
+  console.log("posting,", url, data);
+  const res: Response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) {
+    console.log("Error in postData", { url, data, res });
+    throw Error(res.statusText);
+  }
+  return res.json();
+};
+
+export const toDateTime = (secs: number) => {
+  var t = new Date("1970-01-01T00:30:00Z");
+  t.setSeconds(secs);
+  return t;
+};
